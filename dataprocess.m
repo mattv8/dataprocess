03a449e914fdfa1799fb463620e7fdf46c4d74e5
mattv8 <matt.visnovsk@gmail.com>
2014-10-20 20:43:00 -0600

    Added Features
    -Function now accepts more than 1 file extension
    -If Operation is left undefined the script simply loads the file
    -Updated robustness

--- dataprocess.m
+++ dataprocess.m
@@ -1,20 +1,21 @@
 % 
 % Written by Matthew Visnovsky
-% Contact: matt.visnovsk@gmail.com
+% Contact: matt@visnovsky.us
 % 19 October, 2014
 %
 % USAGE
 %   This function is useful for sifting through folders with delimited data
-%   files (.txt, .dat, etc.) and outputting a cell matirx containing a)the
+%   files (.txt, .dat, etc.) and outputting a cell matrix containing a)the
 %   outputs of the processed data, b)the position of the files of
 %   interest within the data containing folder, and c)a directory listing of 
-%   the data containing folder. Data files will be processed in their 
-%   directory listing sort order (by name, date, etc.).
+%   the data containing folder. Note that data files will be processed in their 
+%   directory listing sort order (whether by name, date, etc.).
 %
 % INPUTS
 %   folder_name: directory of data containing folder 
 %       (eg. 'S:\Program Files\MATLAB\R2014a\data') [string]
-%   extension: name of file extension (eg. ".dat" or ".txt") [string]
+%   extension: cell of strings of the file extension(s) (eg. ".dat" or ".txt") 
+%       [cell array of strings]
 %   keywords: cell of strings, these are the repeated words within the
 %       named data files [cell array of strings]
 %   operation: called as a function, this operation is carried out on every
@@ -25,8 +26,9 @@
 %   DataCell: This is a matrix containing the data that was processed using
 %       the inputted function. The data columns are in order of inputted keywords.
 %   ListCell: This is a cell matrix of strings listing the names of all of the
-%       files within the data containing folder. [cell array of strings]
-%   
+%       files within the data containing folder that are found to contain
+%       the specified key words. [cell array of strings]
+%
 % EXAMPLE USAGE
 %   extension = '.dat';
 %   keywords = {'DYN' 'STA' 'TOT' 'EP'}; 
@@ -36,39 +38,68 @@
 %   [Data,List] = dataprocess(folder_name,extension,keywords,operation);
 %
 
-function [Datacell,ListCell] = dataprocess(folder_name,extension,keywords,operation)
+function [DataCell,ListCell] = dataprocess(folder_name,extension,keywords,operation)
 %Check inputs
-if isempty(extension) == 1
-    error('Please specify filetype extension as a string (eg. ''.dat'')')
-elseif isempty(keywords) == 1
-    error('Please specify key words as strings (eg. ''DAT'')')
+if isempty(extension) == 1, error('Please specify filetype extension as a string (eg. ''.dat'')'),end
+if isempty(keywords) == 1, error('Please specify key words as strings (eg. ''DAT'')'),end
 %Check for proper inputs
-elseif isa(folder_name, 'char') == 0
-    error('Please input folder name as a string (eg. ''S:\Program Files\MATLAB\R2014a\data''')
-elseif isa(operation, 'char') == 1
-    operation = str2func(operation);
-    warning('The specified operation was inputted as a string, and was converted a function handle')
-elseif isa(operation, 'function_handle') == 0
-    error('Inputted operation was not a string or function handle')
-elseif isa(keywords, 'char') == 0
-    error('Inputted keywords were not a string')
+if isa(folder_name, 'char') == 0, error('Please input folder name as a string (eg. ''S:\Program Files\MATLAB\R2014a\data'''),end
+if isa(keywords, 'char') == 0 && isa(keywords, 'cell') == 0, error('Inputted keywords were not a cell or string'), end
 %Check for files
-elseif  isempty(dir(folder_name)) == 1, error('There are no files in this folder')
+if  isempty(dir(folder_name)) == 1, error('There are no files in this folder'),end
+%If operation is specified
+
+if nargin ==4
+    if isa(operation, 'char') == 1
+        operation = str2func(operation);
+    elseif isa(operation, 'function_handle') == 0
+        error('Inputted operation was not a string or function handle')
+    end
 end
 
 %List data containging folder's contents
-ext = strread(sprintf('*%s\n',extension),'%s'); %appends wildcard to beginnging of string
-List = dir(fullfile(folder_name, ext{1})); %creates structure based on folder listing
-List = {List.name}'; %throws out date, filesize, etc. and creates simple char array of filenames
+if  length(extension) > 1 %If more than one extension is specified
+    i=1;
+    for k = 1:length(extension)
+        ext{i} = strread(sprintf('*%s\n',extension{i}),'%s'); %appends wildcard to beginnging of string
+        List{i} = dir(fullfile(folder_name, cell2mat(ext{i}))); %creates structure based on folder listing
+        List{i} = {List{i}.name}'; %throws out date, filesize, etc. and creates simple char array of filenames
+        i=i+1;
+    end
+    
+    %Check for files with proper extension(s)
+    i=1;
+    for k = 1:length(extension)
+        if isempty(List{i}) == 1, error('There are no files with the extension "%s" in %s',ext{i},folder_name),end
+    end
+    
+    %Transpose and combine files with extension into single 1xn cell array
+    i=1;
+    for k = length(extension)+1
+        List = [List{i}; List{i+1}];
+        i=i+1;
+    end
+    
+else %If only one extension is specified
+    if isa(extension, 'char') == 1 %if extension is inputted as string
+        ext = strread(sprintf('*%s\n',extension),'%s'); 
+        List = dir(fullfile(folder_name, ext{1})); 
+        List = {List.name}'; 
+    end
+    if isa(extension, 'cell') == 1 %if extension is inputted as cell
+        extension = cell2mat(extension);
+        ext = strread(sprintf('*%s\n',extension),'%s'); 
+        List = dir(fullfile(folder_name, ext{1})); 
+        List = {List.name}';
+    end
+    if isempty(List) == 1, error('There are no files with the extension "%s" in %s',ext{1},folder_name),end
+end
 
-%Check for files with proper extension
-if isempty(List) == 1, error('There are no files with the extension "%s" in %s',ext{1},folder_name), end
-
-j = 1;
+i = 1;
 for k = 1:length(keywords)
-    CellIndex{j} = strfind(List,keywords{j}); %creates a boolean array for filenames containing keyword at i
-    Idx{j} = find(not(cellfun('isempty', CellIndex{j}))); %creates an index array stating location of found files
-    j=j+1;
+    CellIndex{i} = strfind(List,keywords{i}); %creates a boolean array for filenames containing keyword at i
+    Idx{i} = find(not(cellfun('isempty', CellIndex{i}))); %creates an index array stating location of found files
+    i=i+1;
 end
 
 %Convert Idx from cell array to matrix, uneven arrays are padded with NaN's
@@ -79,28 +110,46 @@ Index = cellfun(fcn,Idx,'UniformOutput',false);  % Pad each cell with NaNs
 Index = vertcat(Index{:});                  % Vertically concatenate cells
 Index = Index'; % UnTranspose each cell
 
-%Perform math on Datamat using fcn
-i=1; j=1;
-for i = 1:length(Index) %nested for loop FTW! :D
-    for j = 1:length(keywords)
-        if Index(i,j) > 0
-        directory{i,j} = sprintf('%s\\%s',folder_name,List{Index(i,j)});
-        Datamat(i,j) = operation(load(directory{i,j})); %operation is called as function
-        ListCell{i,j} = List{Index(i,j)}; %saves a new directory listing, isolating files by keyword
-        else Datamat(i,j) = nan();
-        end
-    j=j+1;  %
-    end     %next iteration
-    i=i+1;  %
+%Simply load the files if Operation is not specified. 
+if nargin == 3
+    i=1; j=1;
+    for i = 1:length(Index)
+        for j = 1:length(keywords)
+            if Index(i,j) > 0
+            directory{i,j} = sprintf('%s\\%s',folder_name,List{Index(i,j)});
+            DataCell{i,j} = load(directory{i,j}); %operation is called as function
+            ListCell{i,j} = List{Index(i,j)}; %saves a new directory listing, isolating files by keyword
+            else DataCell{i,j} = nan(); %Uneven cells are padded with NaN's
+            end
+        j=j+1;  %
+        end     %next iteration
+        i=i+1;  %
+    end
 end
 
-%Remove NaN's and convert output back to cell array
-i=size(Datamat,2);
-Datacell = cell(i,1);
-for i = 1:size(Datamat,2);
-    Datacell{i} = Datamat(~isnan(Datamat(:,i)),i);
-end
+%Otherwise perform math on Datamat using Operation
+if nargin == 4 
+    i=1; j=1;
+    for i = 1:length(Index)
+        for j = 1:length(keywords)
+            if Index(i,j) > 0
+            directory{i,j} = sprintf('%s\\%s',folder_name,List{Index(i,j)});
+            Datamat(i,j) = operation(load(directory{i,j})); %operation is called as function
+            ListCell{i,j} = List{Index(i,j)}; %saves a new directory listing, isolating files by keyword
+            else Datamat(i,j) = nan();
+            end
+        j=j+1;  %
+        end     %next iteration
+        i=i+1;  %
+    end
+    %Remove NaN's and convert output back to cell array
+    i=size(Datamat,2);
+    DataCell = cell(i,1);
+    for i = 1:size(Datamat,2);
+    DataCell{i} = Datamat(~isnan(Datamat(:,i)),i);
+    end
 end
 
+end %end function
 
 
